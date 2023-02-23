8595d9c779157403ef3145bab847afb4383172ff
Gordon <e439931@us.lmco.com>
2023-02-23 15:53:25 -0500

    Fix error in today_num. Change inputs to get_aggregated_data to include current sprint

--- Stoplight.py
+++ Stoplight.py
@@ -41,7 +41,7 @@ def main(curSprint, lastCompleteSprint, PI,
     # Clins
     pattern = re.compile("CLIN \d{4}")
     clins = sorted(list(set([re.search(pattern, epic).group() for epic in epics])))
-    data = get_aggregated_data(lastCompleteSprint, 
+    data = get_aggregated_data(curSprint, lastCompleteSprint, 
                                newJiraFile, prevJiraFile, 
                                baseJiraFile, PILookupFile,
                                epics, clins, PI)
@@ -54,7 +54,6 @@ def main(curSprint, lastCompleteSprint, PI,
                  newJiraFile, prevJiraFile, baseJiraFile)
 
     # Separate by CLIN
-    
     stoplight_dict = {}
     for clin in clins:
         stoplight, change_BL = get_stoplight_data(data, clin)
@@ -98,7 +97,7 @@ if __name__ == "__main__":
         last_tuesday = today - dt.timedelta(days=6)
     else:
         # it is not monday
-        last_tuesday = today - dt.timedelta(days=today_num+1)
+        last_tuesday = today - dt.timedelta(days=today_day+1)
     PI_sprint = sprints[((last_tuesday > sprints.Start) & (last_tuesday <= sprints.End))].iloc[0].Sprint
     cur_sprint = PI_sprint.split('.')[-1]
     if cur_sprint == "IP":
@@ -115,18 +114,18 @@ if __name__ == "__main__":
                         help='Path to previous Jira export')
     parser.add_argument('baseJiraFile', 
                         help='Path to baseline Jira export')
-    parser.add_argument('-sprint', 
+    parser.add_argument('--sprint', 
                         help='INT: Current sprint number', 
                         type=int,
                         default=cur_sprint)
-    parser.add_argument('-lastCompleteSprint', 
+    parser.add_argument('--lastCompleteSprint', 
                         help='INT: Last complete sprint number', 
                         type=int,
                         default=last_complete_sprint)
-    parser.add_argument('-PILookupFile', 
+    parser.add_argument('--PILookupFile', 
                         help='Path to PI Lookup file with dates for each PI', 
                         default=default_PILookupFile)
-    parser.add_argument('-stoplightWdir', 
+    parser.add_argument('--stoplightWdir', 
                         help='Working directory where Stoplight files will be output', 
                         default=default_stoplight_wdir)
     args= parser.parse_args()
